a6ce4c97e8dc69dfc410cb03cadeb329c9eb1e50
Gukson <75785907+Gukson@users.noreply.github.com>
2023-03-05 19:20:55 +0100

    list3 made

--- listy/Programy/snipped/lista3/Triangle.java
+++ listy/Programy/snipped/lista3/Triangle.java
@@ -6,15 +6,17 @@ public class Triangle {
     public static void main(String[] args) {
         Scanner userInterface = new Scanner(System.in);
         int N = userInterface.nextInt();
+        print_triangle(N);
     }
-    public static int fibbR(int n){
-        if(n<3) return 1;
-        return fibbR(n-2)+ fibbR(n-1);
+    public static int fibb(int n, int k){
+        if(k == 0 || k == n) return 1;
+        return fibb(n-1, k-1) + fibb(n-1,k);
     }
     public static void print_triangle(int N){
-
-//        for(int x = 0; x<N;x++){
-//
-//        }
+        for(int x = 0; x<N;x++){
+            for(int y=0;y<(N-x)*2;y++) System.out.print(" ");
+            for(int y = 0;y<=x;y++) System.out.print(fibb(x,y)+"   ");
+            System.out.println();
+        }
     }
 }
