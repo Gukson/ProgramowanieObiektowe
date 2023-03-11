cd6fc49ae1fe4156ec80c7866b4643a1878f4f1f
Gukson <75785907+Gukson@users.noreply.github.com>
2023-03-11 17:01:34 +0100

    repaired

--- listy/Programy/snipped/lista3/Triangle.java
+++ listy/Programy/snipped/lista3/Triangle.java
@@ -1,21 +1,36 @@
 package snipped.lista3;
 
-import java.util.Scanner;
-
 public class Triangle {
     public static void main(String[] args) {
-        Scanner userInterface = new Scanner(System.in);
-        int N = userInterface.nextInt();
-        print_triangle(N);
+        print_triangle(4);
+
     }
     public static int fibb(int n, int k){
         if(k == 0 || k == n) return 1;
         return fibb(n-1, k-1) + fibb(n-1,k);
     }
-    public static void print_triangle(int N){
+
+    public static void generate_numbers(int N, int tab[]){
+        int k = 0;
         for(int x = 0; x<N;x++){
-            for(int y=0;y<(N-x)*2;y++) System.out.print(" ");
-            for(int y = 0;y<=x;y++) System.out.print(fibb(x,y)+"   ");
+            for(int y = 0;y<=x;y++) {
+                tab[k] = fibb(x, y);
+                k++;
+            }
+        }
+    }
+
+    public static void print_triangle(int N){
+        int [] liczby = new int[1000];
+        generate_numbers(N,liczby);
+
+        int k = 0;
+        for(int x = 0; x<N;x++){
+            for(int y=0;y<(N-x)*2;y++) System.out.print("0");
+            for(int y = 0;y<=x;y++) {
+                System.out.print("00"+liczby[k] );
+                k++;
+            }
             System.out.println();
         }
     }
