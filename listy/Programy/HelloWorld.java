9d1713f737533505362855d530efe3d00b2dd04e
Gukson <75785907+Gukson@users.noreply.github.com>
2023-05-22 01:54:36 +0200

    lol

--- listy/Programy/HelloWorld.java
+++ listy/Programy/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+<<<<<<< Updated upstream:listy/Programy/HelloWorld.java
 
+=======
+        System.out.println("Siema");
+>>>>>>> Stashed changes:Programy/zajęcia1/HalloWorld.java
     }
 }
